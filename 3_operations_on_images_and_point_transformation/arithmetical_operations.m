cbaea03e42ee4cc39a4b6a9ae1f2e8aeaeed07e9
szuzanna <zdrapka111@gmail.com>
2019-10-24 13:08:49 +0200

    Logical operations & arithmetical operation

---
+++ 3_operations_on_images_and_point_transformation/arithmetical_operations.m
@@ -0,0 +1,57 @@
+clearvars;
+close all;
+clc;
+
+img1 = imread('lena.bmp');
+img2 = imread('jet.bmp');
+
+%% addition two images
+result1= imadd(img1,img2);
+figure(1);
+imshow(result1);
+
+%% addition with specific type
+result11= imadd(img1,img2,'uint16');
+figure(11);
+imshow(result11,[]);
+
+%% linear combination 
+result2= imlincomb(1,img1,1,img2,'uint16');
+figure(2);
+imshow(result2,[]);
+
+%% substraction 
+result12= imsubtract(int16(img1),int16(img2));
+figure(12);
+imshow(result12,[]);
+
+%% absolute subtraction
+result3= imabsdiff(img1,img2);
+figure(3);
+imshow(result3);
+
+%% multiplication by a constant
+result4= immultiply(img1,0.5);
+figure(4);
+imshow(result4);
+
+%% multiplication by mask
+circle = imread('kolo.bmp');
+mask = boolean(circle);
+result5= immultiply(img1,mask);
+figure(5);
+imshow(result5);
+
+%% negative
+result6= imcomplement(img1);
+figure(6);
+imshow(result6);
+
+
+
+
+
+
+
+
+
---
+++ 3_operations_on_images_and_point_transformation/logical_operations.m
@@ -0,0 +1,28 @@
+clearvars;
+close all;
+clc;
+
+img10 = imread('kolo.bmp');
+img20 = imread('kwadrat.bmp');
+
+img1 = boolean(img10);
+img2  = boolean(img20);
+%% NOT
+result1= ~img1;
+figure(1);
+imshow(result1);
+
+%% AND
+result2= img1 & img2;
+figure(2);
+imshow(result2);
+
+%% OR
+result3= img1 | img2;
+figure(3);
+imshow(result3);
+
+%% XOR
+result4= xor(img1,img2);
+figure(4);
+imshow(result4);
